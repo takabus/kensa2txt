e9a58de966428698d707acfbbb2f2905a15914e1
t.h <zetros1833@gmail.com>
2021-09-28 20:43:51 +0900

    init

---
+++ kensastr/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace kensastr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnCopy = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 228F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 77.41312F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(550, 507);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 2);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(318, 503);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.button1);
+            this.flowLayoutPanel1.Controls.Add(this.button2);
+            this.flowLayoutPanel1.Controls.Add(this.textBox1);
+            this.flowLayoutPanel1.Controls.Add(this.btnCopy);
+            this.flowLayoutPanel1.Controls.Add(this.btnHelp);
+            this.flowLayoutPanel1.Controls.Add(this.label1);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(324, 2);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(224, 503);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button1.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button1.Location = new System.Drawing.Point(2, 2);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "テキスト生成(改行なし)";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.button2.Font = new System.Drawing.Font("MS UI Gothic", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.button2.Location = new System.Drawing.Point(2, 40);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(220, 34);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "テキスト生成(改行あり)";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textBox1.Location = new System.Drawing.Point(2, 78);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(220, 259);
+            this.textBox1.TabIndex = 3;
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Location = new System.Drawing.Point(3, 342);
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(100, 23);
+            this.btnCopy.TabIndex = 5;
+            this.btnCopy.Text = "テキストコピー";
+            this.btnCopy.UseVisualStyleBackColor = true;
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(109, 342);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(105, 23);
+            this.btnHelp.TabIndex = 7;
+            this.btnHelp.Text = "ヘルプ";
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelp_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 368);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(213, 12);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "使用方法についてはヘルプを参照してください";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 507);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "血液検査データ 入力補助ツール";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnCopy;
+        private System.Windows.Forms.Button btnHelp;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ kensastr/Form1.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic.FileIO;
+
+namespace kensastr
+{
+    public partial class Form1 : Form
+    {
+        static DataTable dataTable;
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+        //https://www.oborodukiyo.info/Forms/VS2008/F-ReadCSVToDataTable
+        //dt:データを入れるDataTable
+        //hasHeader:CSVの一行目がカラム名かどうか
+        //fileName:ファイル名
+        //separator:カラムを分けている文字(,など)
+        //quote:カラムを囲んでいる文字("など)
+        private void ReadCSV(DataTable dt, bool hasHeader, string fileName, string separator, bool quote)
+        {
+            //CSVを便利に読み込んでくれるTextFieldParserを使います。
+            TextFieldParser parser = new TextFieldParser(fileName, Encoding.GetEncoding("shift_jis"));
+            //これは可変長のフィールドでフィールドの区切りのマーカーが使われている場合です。
+            //フィールドが固定長の場合は
+            //parser.TextFieldType = FieldType.FixedWidth;
+            parser.TextFieldType = FieldType.Delimited;
+            //区切り文字を設定します。
+            parser.SetDelimiters(separator);
+            //クォーテーションがあるかどうか。
+            //但しダブルクォーテーションにしか対応していません。シングルクォーテーションは認識しません。
+            parser.HasFieldsEnclosedInQuotes = quote;
+            string[] data;
+            //ここのif文では、DataTableに必要なカラムを追加するために最初に1行だけ読み込んでいます。
+            //データがあるか確認します。
+            if (!parser.EndOfData)
+            {
+                //CSVファイルから1行読み取ります。
+                data = parser.ReadFields();
+                //カラムの数を取得します。
+                int cols = data.Length;
+                if (hasHeader)
+                {
+                    for (int i = 0; i < cols; i++)
+                    {
+                        dt.Columns.Add(new DataColumn(data[i]));
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < cols; i++)
+                    {
+                        //カラム名にダミーを設定します。
+                        dt.Columns.Add(new DataColumn());
+                    }
+                    //DataTableに追加するための新規行を取得します。
+                    DataRow row = dt.NewRow();
+                    for (int i = 0; i < cols; i++)
+                    {
+                        //カラムの数だけデータをうつします。
+                        row[i] = data[i];
+                    }
+                    //DataTableに追加します。
+                    dt.Rows.Add(row);
+                }
+            }
+            //ここのループがCSVを読み込むメインの処理です。
+            //内容は先ほどとほとんど一緒です。
+            while (!parser.EndOfData)
+            {
+                data = parser.ReadFields();
+                DataRow row = dt.NewRow();
+                for (int i = 0; i < dt.Columns.Count; i++)
+                {
+                    row[i] = data[i];
+                }
+                dt.Rows.Add(row);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataTable = new DataTable();
+
+            Form1 form1 = new Form1();
+            form1.ReadCSV(dataTable, false, "master.csv", ",", false);
+            dataGridView1.DataSource = dataTable;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            dataTable = (DataTable)dataGridView1.DataSource;
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                if (dataTable.Rows[i][1].ToString() != "")
+                {
+                    stringBuilder.Append(dataTable.Rows[i][0]);
+                    stringBuilder.Append("");
+                    stringBuilder.Append(dataTable.Rows[i][1]);
+                    stringBuilder.Append(dataTable.Rows[i][2]);
+                    stringBuilder.Append(",");
+                }
+            }
+
+            string str = stringBuilder.ToString();
+            this.textBox1.Text = str;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            dataTable = (DataTable)dataGridView1.DataSource;
+
+            StringBuilder stringBuilder = new StringBuilder();
+
+            for (int i = 0; i < dataTable.Rows.Count; i++)
+            {
+                if (dataTable.Rows[i][1].ToString() != "")
+                {
+                    stringBuilder.Append(dataTable.Rows[i][0]);
+                    stringBuilder.Append("");
+                    stringBuilder.Append(dataTable.Rows[i][1]);
+                    stringBuilder.Append(dataTable.Rows[i][2]);
+                    stringBuilder.Append("\r\n");
+                }
+
+            }
+            string str = stringBuilder.ToString();
+            this.textBox1.Text = str;
+
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(this.textBox1.Text);
+        }
+
+        private void btnHelp_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("help.html");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
